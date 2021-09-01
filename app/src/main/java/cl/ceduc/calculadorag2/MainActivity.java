2a178079a9f9d60d6c31189c61ce438eb207a439
Leyla <leylasq90@gmail.com>
2021-09-01 14:57:16 -0400

    Modificacion operaciones calculadora

--- app/src/main/java/cl/ceduc/calculadorag2/MainActivity.java
+++ app/src/main/java/cl/ceduc/calculadorag2/MainActivity.java
@@ -1,148 +1,157 @@
 package cl.ceduc.calculadorag2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btn_add, btn_divide, btn_subtract, btn_multiply, btn_backspace, btn_clear, btn_decimal, btn_equal, btn_zero, btn_one, btn_two, btn_three, btn_four, btn_five, btn_six, btn_seven, btn_eight, btn_nine;
-    private TextView txt_display;
-    private double num1, num2, resultado;
-    private String operador;
+    Button btn_zero, btn_one, btn_two, btn_three, btn_four, btn_five, btn_six, btn_seven, btn_eight, btn_nine, btn_decimal, btn_clear, btn_delete, btn_add, btn_subtract, btn_multiply,btn_divide, btn_equal;
+    TextView txt_display;
+    double num1, num2, digito, resultado;
+    String operador, actual, nuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Asignamos los botones numericos
-        Button btn_zero = (Button) this.findViewById(R.id.btn_zero);
-        Button btn_one = (Button) this.findViewById(R.id.btn_one);
-        Button btn_two = (Button) this.findViewById(R.id.btn_two);
-        Button btn_three = (Button) this.findViewById(R.id.btn_three);
-        Button btn_four = (Button) this.findViewById(R.id.btn_four);
-        Button btn_five = (Button) this.findViewById(R.id.btn_five);
-        Button btn_six = (Button) this.findViewById(R.id.btn_six);
-        Button btn_seven = (Button) this.findViewById(R.id.btn_seven);
-        Button btn_eight = (Button) this.findViewById(R.id.btn_eight);
-        Button btn_nine = (Button) this.findViewById(R.id.btn_nine);
+        btn_zero = (Button) findViewById(R.id.btn_zero);
+        btn_one = (Button) findViewById(R.id.btn_one);
+        btn_two = (Button) findViewById(R.id.btn_two);
+        btn_three = (Button) findViewById(R.id.btn_three);
+        btn_four = (Button) findViewById(R.id.btn_four);
+        btn_five = (Button) findViewById(R.id.btn_five);
+        btn_six = (Button) findViewById(R.id.btn_six);
+        btn_seven = (Button) findViewById(R.id.btn_seven);
+        btn_eight = (Button) findViewById(R.id.btn_eight);
+        btn_nine = (Button) findViewById(R.id.btn_nine);
+        btn_decimal = (Button) findViewById(R.id.btn_decimal);
         //Asignamos los botones operacionales
-        Button btn_add = (Button) this.findViewById(R.id.btn_add);
-        Button btn_divide = (Button) this.findViewById(R.id.btn_divide);
-        Button btn_multiply = (Button) this.findViewById(R.id.btn_multiply);
-        Button btn_subtract = (Button) this.findViewById(R.id.btn_subtract);
-        Button btn_backspace = (Button) this.findViewById(R.id.btn_backspace);
-        Button btn_clear = (Button) this.findViewById(R.id.btn_clear);
-        Button btn_decimal = (Button) this.findViewById(R.id.btn_decimal);
-        Button btn_equal = (Button) this.findViewById(R.id.btn_equal);
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+        btn_delete = (Button) findViewById(R.id.btn_delete);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        btn_subtract = (Button) findViewById(R.id.btn_subtract);
+        btn_multiply = (Button) findViewById(R.id.btn_multiply);
+        btn_divide = (Button) findViewById(R.id.btn_divide);
+        btn_equal = (Button) findViewById(R.id.btn_equal);
         //Asignamos la pantalla
-        TextView txt_display = (TextView) this.findViewById(R.id.txt_display);
+        txt_display = (TextView) findViewById(R.id.txt_display);
 
-        btn_zero.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("0");}
-        });
-        btn_one.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("1");}
-        });
-        btn_two.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("2");}
-        });
-        btn_three.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("3");}
-        });
-        btn_four.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("4");}
-        });
-        btn_five.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("5");}
-        });
-        btn_six.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("6");}
-        });
-        btn_seven.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("7");}
-        });
-        btn_eight.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("8");}
-        });
-        btn_nine.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setDigito("9");}
-        });
+        btn_zero.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("0");}});
+        btn_one.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("1");}});
+        btn_two.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("2");}});
+        btn_three.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("3");}});
+        btn_four.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("4");}});
+        btn_five.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("5");}});
+        btn_six.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("6");}});
+        btn_seven.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("7");}});
+        btn_eight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("8");}});
+        btn_nine.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {setDigito("9");}});
         btn_decimal.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {setDigito("."); }
-        });
+            public void onClick(View v) {setDigito(".");}});
 
-
-        btn_divide.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {OnClickOperacionNum1();}
-        });
-        btn_multiply.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setOperacion();}
-        });
+        //Botones Operacionales
+        btn_clear.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {setClear();}});
+        btn_delete.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {setDelete();}});
         btn_add.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setOperacion(add("+"););}
-        });
+            public void onClick(View v) {
+                setOperacion();
+                operador = "+";
+            }});
         btn_subtract.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {setOperacion("-");}
-        });
-        btn_clear.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v) {setClear("C");}
-        });
-        btn_equal.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v) {setEqual("=");}
-        });
+            public void onClick(View v) {
+                setOperacion();
+                operador = "-";
+            }});
+        btn_multiply.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                setOperacion();
+                operador = "*";
+            }});
+        btn_divide.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                setOperacion();
+                operador = "/";
+            }});
+        btn_equal.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                setEqual();
+            }});
     }
 
-    void setDigito(String digito){
-        TextView txt_display = (TextView) this.findViewById(R.id.txt_display);
-        String actual = txt_display.getText().toString();
-        String nuevo = actual + digito;
-        txt_display.setText(nuevo);
-    }
-    void setClear (String clear){
-        TextView txt_display = (TextView) this.findViewById(R.id.txt_display);
-        num1 = 0;
-        num2 = 0;
-        txt_display.setText("");
-    }
-    public void setOperacion (String operacion){
+
+    public void setDigito(String ingresonum) {
         txt_display = (TextView) this.findViewById(R.id.txt_display);
-        num1=Double.parseDouble(txt_display.getText().toString());
-        txt_display.setText(" ");
+        digito = Double.parseDouble(txt_display.getText().toString());
+            if (digito == 0.0) {
+                txt_display.setText(ingresonum);
+            }else{
+                actual = txt_display.getText().toString();
+                nuevo = actual + ingresonum;
+                txt_display.setText(nuevo);
+            }
+        // borrar numero despues de una operacion, sino siguen los numeros agregandose
+        // restringir punto decimal (solo uno por digito)
     }
-    public void add (View view){
-        operador = "+";
-        setOperacion(" ");
+
+    public void setOperacion() {
+        txt_display = (TextView) this.findViewById(R.id.txt_display);
+        num1 = Double.parseDouble(txt_display.getText().toString());
+        txt_display.setText("0");
     }
-    public void subtract (View view){
-        operador = "-";
-        OnClickOperacionNum1(view);
-    }
-    public void multiply (View view){
-        operador = "*";
-        OnClickOperacionNum1(view);
-    }
-    public void divide (View view){
-        operador = "/";
-        OnClickOperacionNum1(view);
-    }
-    public void setEqual (View v){
+
+    public void setEqual() {
         txt_display = (TextView) this.findViewById(R.id.txt_display);
         num2=Double.parseDouble(txt_display.getText().toString());
-        if (operador.equals("+")){
-            resultado=num1+num2;
-        }else if (operador.equals("-")){
-            resultado=num1-num2;
-        }else if (operador.equals("*")){
-            resultado=num1*num2;
-        }else if (operador.equals("/")){
-            resultado=num1/num2;
-        }
-        txt_display.setText(" "+resultado);
+        if (operador.equals("+")) {
+            resultado = num1 + num2;
+        }else if (operador.equals("-")) {
+            resultado = num1 - num2;
+        }else if (operador.equals("*")) {
+            resultado = num1 * num2;
+        }else if (operador.equals("/")) {
+            if (num2 == 0){
+                txt_display.setText("0");
+                Toast.makeText(this,"Operación no válida",Toast.LENGTH_LONG).show();
+            }else{
+                resultado = num1 / num2;}}
+        txt_display.setText(resultado + "");
+
+        num1 = 0;
+        num2 = 0;
+        operador = "";
+
+        // numero final sale en double, sacar el ,0 y arreglar que se pueden seguir ingresando numeros
     }
 
+    public void setClear() {
+        txt_display = (TextView) this.findViewById(R.id.txt_display);
+        num1 = 0;
+        num2 = 0;
+        operador = "";
+        txt_display.setText("0");
+    }
 
- }
+    public void setDelete (){
+        if(txt_display.getText().toString().equals(" ")){
+            txt_display.setText(txt_display.getText().subSequence(0,txt_display.getText().length()-1)+" ");
+        }
+    }
+}
